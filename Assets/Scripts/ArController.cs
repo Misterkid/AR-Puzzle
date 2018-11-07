55dcf76502dee9f42ae17cfaba95e22ddff298d0
Quget <eddymeivogel@gmail.com>
2018-11-07 09:09:38 +0100

    Added AR Core, Made a test script

---
+++ Assets/Scripts/ArController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+namespace ARPuzzle
+{
+    public class ArController : MonoBehaviour
+    {
+
+        List<DetectedPlane> detectedPlanes = new List<DetectedPlane>();
+
+        private List<DetectedPlane> floorPlanes = new List<DetectedPlane>();
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            DetectFloor();
+            //Session.GetTrackables<DetectedPlane>(detectedPlanes);
+        }
+
+        private void DetectObjects()
+        {
+            Debug.Log("ToDo");
+        }
+
+        private void DetectFloor()
+        {
+            Session.GetTrackables<DetectedPlane>(detectedPlanes);
+            for (int i = 0; i < detectedPlanes.Count; i++)
+            {
+                Debug.Log(detectedPlanes[i].TrackingState);
+                Debug.Log(detectedPlanes[i].CenterPose);
+            }
+        }
+    }
+}
